889950628a3e3c7d0ac8c239a741e4182e10cda6
Angel3304 <aluisl1900@alumno.ipn.mx>
2026-01-11 13:57:34 -0600

    prueba

--- Memory_Store_RAM.vhd
+++ Memory_Store_RAM.vhd
@@ -12,147 +12,162 @@ entity Memory_Store_RAM is
 end entity Memory_Store_RAM;
 
 architecture Behavioral of Memory_Store_RAM is
-  -- OPCODES
+
+  -- OPCODES (Añadimos DIV y ADD)
   constant OP_LDX   : std_logic_vector(7 downto 0) := x"01";
   constant OP_LDY   : std_logic_vector(7 downto 0) := x"02";
-  constant OP_ADD   : std_logic_vector(7 downto 0) := x"03";
+  constant OP_ADD   : std_logic_vector(7 downto 0) := x"03"; -- ¡Nuevo! Para mover Y a X
   constant OP_CMP   : std_logic_vector(7 downto 0) := x"05"; 
   constant OP_DISP  : std_logic_vector(7 downto 0) := x"06";
   constant OP_JUMP  : std_logic_vector(7 downto 0) := x"07";
   constant OP_BR_NZ : std_logic_vector(7 downto 0) := x"08"; 
-  constant OP_SUB   : std_logic_vector(7 downto 0) := x"09";
   constant OP_WAIT  : std_logic_vector(7 downto 0) := x"0A";
-  constant OP_DIV   : std_logic_vector(7 downto 0) := x"10"; 
+  constant OP_DIV   : std_logic_vector(7 downto 0) := x"10"; -- ¡Nuevo! División HW
   constant OP_STX   : std_logic_vector(7 downto 0) := x"11";
   constant OP_LDI   : std_logic_vector(7 downto 0) := x"12"; 
   constant OP_LDIY  : std_logic_vector(7 downto 0) := x"13";
-  constant OP_RAND  : std_logic_vector(7 downto 0) := x"14"; -- NUEVO OPCODE
 
   type t_mem_array is array (0 to 255) of std_logic_vector(7 downto 0);
 
-  -- MAPA DE VARIABLES
-  constant R_SALDO     : std_logic_vector(7 downto 0) := x"F0";
-  constant W_SALDO     : std_logic_vector(7 downto 0) := x"F2"; 
-  constant R_MONTO     : std_logic_vector(7 downto 0) := x"F4"; 
-  constant W_MONTO     : std_logic_vector(7 downto 0) := x"F6"; 
-  constant R_ELECCION  : std_logic_vector(7 downto 0) := x"F8"; 
-  constant W_ELECCION  : std_logic_vector(7 downto 0) := x"FA"; 
-  constant R_RESULTADO : std_logic_vector(7 downto 0) := x"FC"; 
-  constant W_RESULTADO : std_logic_vector(7 downto 0) := x"FE"; 
+  -- MAPA DE MEMORIA (Movido a zona xB0 para dar espacio al código nuevo)
+  constant R_SALDO     : std_logic_vector(7 downto 0) := x"B0"; -- 176
+  constant W_SALDO     : std_logic_vector(7 downto 0) := x"B2"; 
+
+  constant R_APUESTA   : std_logic_vector(7 downto 0) := x"B3";
+  constant W_APUESTA   : std_logic_vector(7 downto 0) := x"B5";
+
+  constant R_RESULTADO : std_logic_vector(7 downto 0) := x"B6";
+  constant W_RESULTADO : std_logic_vector(7 downto 0) := x"B8";
 
   constant ROM_CODE : t_mem_array := (
-    -- 1. INICIALIZACIÓN
+    -- 1. INICIALIZACIÓN (Dir 0)
+    -- =========================================================
     0 => OP_LDI, 1 => x"32", 2 => x"00",      
     3 => OP_STX, 4 => W_SALDO, 5 => x"00",      
-    6 => OP_LDI, 7 => x"01", 8 => x"00",
-    9 => OP_STX, 10 => W_MONTO, 11 => x"00",
-    12 => OP_LDI, 13 => x"00", 14 => x"00",
-    15 => OP_STX, 16 => W_ELECCION, 17 => x"00",
-
-    -- 2. BARRERA DE SEGURIDAD
-    18 => OP_LDIY, 19 => x"00", 20 => x"00", 
-    21 => OP_LDX, 22 => x"F0", 23 => x"00", 
-    24 => OP_CMP, 25 => x"00", 26 => x"00", 
-    27 => OP_BR_NZ, 28 => x"15", 29 => x"00", 
-
-    -- 3. MENÚ PRINCIPAL
-    30 => OP_LDX, 31 => R_SALDO, 32 => x"00",      
-    33 => OP_DISP, 34 => x"00", 35 => x"00",   
-    36 => OP_LDI, 37 => x"10", 38 => x"00",
-    39 => OP_STX, 40 => x"D0", 41 => x"00", 
-
-    -- LEER INPUT
-    42 => OP_LDX, 43 => x"F0", 44 => x"00", 
-    45 => OP_LDIY, 46 => x"00", 47 => x"00", 
-    48 => OP_CMP, 49 => x"00", 50 => x"00",
-    51 => OP_BR_NZ, 52 => x"39", 53 => x"00",
-    54 => OP_JUMP, 55 => x"2A", 56 => x"00",
-
-    -- 4. PROCESAR TECLA
-    57 => OP_LDIY, 58 => x"0F", 59 => x"00", 
-    60 => OP_CMP, 61 => x"00", 62 => x"00",
-    63 => OP_BR_NZ, 64 => x"45", 65 => x"00",
-    66 => OP_JUMP, 67 => x"78", 68 => x"00", -- JUGAR
-
-    -- Checar A
-    69 => OP_LDIY, 70 => x"0A", 71 => x"00", 
-    72 => OP_CMP, 73 => x"00", 74 => x"00",
-    75 => OP_BR_NZ, 76 => x"57", 77 => x"00",
-    78 => OP_LDI, 79 => x"00", 80 => x"00",   
-    81 => OP_STX, 82 => W_ELECCION, 83 => x"00",
-    84 => OP_JUMP, 85 => x"6F", 86 => x"00",
-
-    -- Checar B
-    87 => OP_LDIY, 88 => x"0B", 89 => x"00", 
-    90 => OP_CMP, 91 => x"00", 92 => x"00",
-    93 => OP_BR_NZ, 94 => x"69", 95 => x"00",
-    96 => OP_LDI, 97 => x"01", 98 => x"00",   
-    99 => OP_STX, 100 => W_ELECCION, 101 => x"00",
-    102 => OP_JUMP, 103 => x"6F", 104 => x"00",
-
-    -- Monto
-    105 => OP_STX, 106 => W_MONTO, 107 => x"00", 
-    108 => OP_JUMP, 109 => x"6F", 110 => x"00", 
-
-    -- Feedback
-    111 => OP_DISP, 112 => x"00", 113 => x"00", 
-    114 => OP_WAIT, 115 => x"00", 116 => x"00", 
-    117 => OP_JUMP, 118 => x"2A", 119 => x"00",
-
-    -- 5. JUEGO: GIRAR RULETA
-    120 => OP_LDI, 121 => x"20", 122 => x"00", 
-    123 => OP_STX, 124 => x"D0", 125 => x"00", 
-    126 => OP_WAIT, 127 => x"00", 128 => x"00", 
-    129 => OP_WAIT, 130 => x"00", 131 => x"00",
-
-    -- GENERAR RESULTADO (USANDO NUEVO OP_RAND)
-    132 => OP_RAND, 133 => x"00", 134 => x"00", -- <--- AQUÍ ESTÁ LA SOLUCIÓN
     
-    135 => OP_LDIY, 136 => x"26", 137 => x"00", -- Div 38
-    138 => OP_DIV, 139 => x"00", 140 => x"00", 
-    141 => OP_LDI, 142 => x"00", 143 => x"00", 
-    144 => OP_ADD, 145 => x"00", 146 => x"00", 
-    147 => OP_STX, 148 => W_RESULTADO, 149 => x"00",
-    150 => OP_DISP, 151 => x"00", 152 => x"00", 
+    6 => OP_LDI, 7 => x"10", 8 => x"00",
+    9 => OP_STX, 10 => x"D0", 11 => x"00", 
 
-    -- 6. VERIFICAR: ¿PAR O IMPAR?
-    153 => OP_LDIY, 154 => x"02", 155 => x"00", 
-    156 => OP_DIV, 157 => x"00", 158 => x"00",  
-    159 => OP_LDI, 160 => x"00", 161 => x"00", 
-    162 => OP_ADD, 163 => x"00", 164 => x"00", 
-    165 => OP_LDY, 166 => R_ELECCION, 167 => x"00", 
-    168 => OP_CMP, 169 => x"00", 170 => x"00", 
-    171 => OP_BR_NZ, 172 => x"C3", 173 => x"00", -- PERDER
+    -- 2. BARRERA DE SEGURIDAD (Dir 12)
+    -- =========================================================
+    12 => OP_LDIY, 13 => x"00", 14 => x"00", 
+    15 => OP_LDX, 16 => x"F0", 17 => x"00", 
+    18 => OP_CMP, 19 => x"00", 20 => x"00", 
+    21 => OP_BR_NZ, 22 => x"0C", 23 => x"00", -- Loop a 12
 
-    -- 7. GANAR
-    174 => OP_LDI, 175 => x"40", 176 => x"00", 
-    177 => OP_STX, 178 => x"D0", 179 => x"00",
-    180 => OP_LDX, 181 => R_SALDO, 182 => x"00", 
-    183 => OP_LDY, 184 => R_MONTO, 185 => x"00", 
-    186 => OP_ADD, 187 => x"00", 188 => x"00", 
-    189 => OP_JUMP, 190 => x"D2", 191 => x"00", 
+    -- 3. MENÚ PRINCIPAL (Dir 24)
+    -- =========================================================
+    24 => OP_LDX, 25 => R_SALDO, 26 => x"00",      
+    27 => OP_DISP, 28 => x"00", 29 => x"00",   
 
-    -- 8. PERDER
-    195 => OP_LDI, 196 => x"50", 197 => x"00", 
-    198 => OP_STX, 199 => x"D0", 200 => x"00",
-    201 => OP_LDX, 202 => R_SALDO, 203 => x"00", 
-    204 => OP_LDY, 205 => R_MONTO, 206 => x"00", 
-    207 => OP_SUB, 208 => x"00", 209 => x"00", 
+    -- LEER INPUT (Dir 30)
+    30 => OP_LDX, 31 => x"F0", 32 => x"00", 
+    33 => OP_LDIY, 34 => x"00", 35 => x"00", 
+    36 => OP_CMP, 37 => x"00", 38 => x"00",
+    39 => OP_BR_NZ, 40 => x"2D", 41 => x"00", -- Ir a PROCESAR (Dir 45)
+    42 => OP_JUMP, 43 => x"1E", 44 => x"00",  -- Loop a LEER (Dir 30)
 
-    -- 9. FIN
-    210 => OP_STX, 211 => W_SALDO, 212 => x"00", 
-    213 => OP_DISP, 214 => x"00", 215 => x"00", 
-    216 => OP_WAIT, 217 => x"00", 218 => x"00", 
-    219 => OP_WAIT, 220 => x"00", 221 => x"00", 
+    -- PROCESAR TECLA (Dir 45 / x2D)
+    -- Checar '#'
+    45 => OP_LDIY, 46 => x"0F", 47 => x"00", 
+    48 => OP_CMP, 49 => x"00", 50 => x"00",  
+    51 => OP_BR_NZ, 52 => x"39", 53 => x"00", -- Ir a GUARDAR APUESTA (Dir 57)
+    54 => OP_JUMP, 55 => x"45", 56 => x"00",  -- Ir a GIRAR (Dir 69)
 
-    -- 10. ENFRIAMIENTO SEGURO
-    222 => OP_LDIY, 223 => x"00", 224 => x"00",
-    225 => OP_LDX, 226 => x"F0", 227 => x"00",
-    228 => OP_WAIT, 229 => x"00", 230 => x"00", 
-    231 => OP_CMP, 232 => x"00", 233 => x"00",
-    234 => OP_BR_NZ, 235 => x"DE", 236 => x"00", 
-    237 => OP_JUMP, 238 => x"1E", 239 => x"00",
+    -- GUARDAR APUESTA (Dir 57 / x39)
+    57 => OP_STX, 58 => W_APUESTA, 59 => x"00", 
+    60 => OP_DISP, 61 => x"00", 62 => x"00", 
+    63 => OP_WAIT, 64 => x"00", 65 => x"00", 
+    66 => OP_JUMP, 67 => x"1E", 68 => x"00", -- Volver a LEER (Dir 30)
+
+    -- 4. JUEGO Y GIRO (Dir 69 / x45)
+    -- =========================================================
+    69 => OP_LDI, 70 => x"20", 71 => x"00", 
+    72 => OP_STX, 73 => x"D0", 74 => x"00",
+    
+    75 => OP_WAIT, 76 => x"00", 77 => x"00", 
+    78 => OP_WAIT, 79 => x"00", 80 => x"00",
+
+    -- =========================================================
+    -- GENERACIÓN DE RESULTADO CON DIVISIÓN HW (Dir 81)
+    -- =========================================================
+    -- 1. Obtener Aleatorio Crudo (0-255) en X
+    81 => OP_LDX, 82 => x"E1", 83 => x"00", 
+    
+    -- 2. Cargar Divisor 38 (x26) en Y
+    84 => OP_LDIY, 85 => x"26", 86 => x"00", 
+    
+    -- 3. DIVIDIR (X / Y). El Residuo queda en Y
+    87 => OP_DIV, 88 => x"00", 89 => x"00", 
+    
+    -- 4. MOVER RESIDUO (Y) A (X)
+    -- X = 0 + Y
+    90 => OP_LDI, 91 => x"00", 92 => x"00", -- X = 0
+    93 => OP_ADD, 94 => x"00", 95 => x"00", -- X = X + Y
+    
+    -- Ahora X tiene un número 0-37 válido.
+    
+    -- 5. Mostrar y Guardar
+    96 => OP_DISP, 97 => x"00", 98 => x"00", 
+    99 => OP_STX, 100 => W_RESULTADO, 101 => x"00", 
+    
+    -- Comparar (Leemos Apuesta)
+    102 => OP_LDY, 103 => R_APUESTA, 104 => x"00", 
+    105 => OP_CMP, 106 => x"00", 107 => x"00", 
+    108 => OP_BR_NZ, 109 => x"7B", 110 => x"00", -- Ir a PERDER (Dir 123)
+
+    -- GANASTE (Dir 111)
+    111 => OP_LDI, 112 => x"40", 113 => x"00", 
+    114 => OP_STX, 115 => x"D0", 116 => x"00",
+    117 => OP_LDI, 118 => x"99", 119 => x"00", -- Saldo 99
+    120 => OP_JUMP, 121 => x"84", 122 => x"00", -- Ir a FIN (Dir 132)
+
+    -- PERDISTE (Dir 123 / x7B)
+    123 => OP_LDI, 124 => x"50", 125 => x"00", 
+    126 => OP_STX, 127 => x"D0", 128 => x"00",
+    129 => OP_LDI, 130 => x"0A", 131 => x"00", -- Saldo 10
+
+    -- FIN (Dir 132 / x84)
+    132 => OP_STX, 133 => W_SALDO, 134 => x"00", 
+    135 => OP_WAIT, 136 => x"00", 137 => x"00", 
+
+    -- ENFRIAMIENTO (Dir 138)
+    138 => OP_LDIY, 139 => x"00", 140 => x"00",
+    141 => OP_LDX, 142 => x"F0", 143 => x"00",
+    144 => OP_CMP, 145 => x"00", 146 => x"00",
+    147 => OP_BR_NZ, 148 => x"8A", 149 => x"00", -- Loop a 138 (x8A)
+    
+    -- VOLVER (Dir 150)
+    150 => OP_JUMP, 151 => x"18", 152 => x"00", -- Ir a MENU (Dir 24 / x18)
 
     others => x"00"
   );
-  -- ... Resto del código de memoria igual ...
+
+  signal RAM_DATA : t_mem_array := (others => x"00");
+
+begin
+    -- Memoria Split (Expandida a 170 para cubrir el código extra)
+    process(Addr_in, RAM_DATA) 
+        variable addr_int : integer;
+    begin
+        addr_int := to_integer(unsigned(Addr_in));
+        
+        -- Leemos ROM si es código (Direcciones bajas)
+        if addr_int < 170 then
+            Data_out <= ROM_CODE(addr_int) & ROM_CODE(addr_int + 1) & ROM_CODE(addr_int + 2);
+        else
+            Data_out <= RAM_DATA(addr_int) & RAM_DATA(addr_int + 1) & RAM_DATA(addr_int + 2);
+        end if;
+    end process;
+
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            -- Solo escribir en zona de Datos (>= 170)
+            if we = '1' and to_integer(unsigned(Addr_in)) >= 170 then
+                RAM_DATA(to_integer(unsigned(Addr_in))) <= Data_in(7 downto 0);
+            end if;
+        end if;
+    end process;
+
+end architecture;
